d15beda1afb313abf30271199aadaa94ed1ad051
Mohammad Oloomi <smh.oloomi@gmail.com>
2021-07-25 01:20:10 +1000

    Change dataset split method to sampling. Update comments and code style.

--- code/case_study.R
+++ code/case_study.R
@@ -3,18 +3,20 @@ library(DBI)
 library(data.table)
 library(xgboost)
 library(dplyr)
+library(splitTools)
+library(Metrics)
 
+##----- Utility functions -----##
 
-#####Utility functions
-
-#function converts datasets to DMatrix
-convertyDataToXgBoost <- function (x, target, features = NULL) 
+# Converts datasets to XGBoost Dense Matrix (xgb.DMatrix)
+convertyDataToXgBoost <- function (x, target, features = NULL)
 {
   if (!target %in% names(x$trainData)) {
     stop("A valid target variable name must be provided")
   }
+  # List of feature column names
   if (is.null(features)) {
-    features = names(x$trainData)[!names(x$trainData) %in% 
+    features = names(x$trainData)[!names(x$trainData) %in%
                                     target]
   }
   else {
@@ -32,10 +34,10 @@ convertyDataToXgBoost <- function (x, target, features = NULL)
       tmptarget = y[, target, with = F][[1]]
       if (is.factor(tmptarget)) {
         lev <- unique(unlist(tmptarget))
-        tmptarget <- as.integer(factor(tmptarget, levels = lev)) - 
+        tmptarget <- as.integer(factor(tmptarget, levels = lev)) -
           1
       }
-      tmp = xgb.DMatrix(data.matrix(y[, features, with = F]), 
+      tmp = xgb.DMatrix(data.matrix(y[, features, with = F]),
                         label = tmptarget, missing = 0)
       return(tmp)
     }
@@ -50,28 +52,27 @@ convertyDataToXgBoost <- function (x, target, features = NULL)
 }
 
 
-#function returns pierson correlation between actuals and predictions
-evaluateModel <- function(actuals, preds){
-  
+# Returns Pierson Correlation between actuals and predictions
+evaluateModel <- function(actuals, preds) {
   return(cor(actuals, preds))
 }
 
 
-#####Read and process datasets
+##----- Read and process datasets -----##
 
-#create spark instance
+# Create Spark connection
 sc <- spark_connect(master = "local")
 
-#read parquet files
-
+# Read Parquet files
 drive <- spark_read_parquet(sc, "drive", "data/drive")
 trip <- spark_read_parquet(sc, "trip", "data/trip")
 weather <- spark_read_parquet(sc, "weather", "data/weather")
 accel <- spark_read_csv(sc, "accel", "data/drive_features.csv")
 
-#merge spark tables
-
-df <- dbGetQuery(sc, " WITH temp_trip AS
+# Merge Spark tables
+df <- dbGetQuery(
+  sc,
+  " WITH temp_trip AS
                  (SELECT a.vehicle_id, a.trip_id
                   , min(a.datetime) as min_time
                   , max(a.datetime) as max_time
@@ -107,18 +108,21 @@ df <- dbGetQuery(sc, " WITH temp_trip AS
                   , a.ft_sum_hard_brakes_3_flg_val
                 FROM accel a, temp_trip b
                 WHERE a.trip_id = b.trip_id
-                 ")
+                 "
+)
 
 spark_disconnect(sc)
 
+# Feature engineering
 
-#####Feature engineering
+# Normalisation of features based on trip durtion
+df$duration <- as.numeric((df$max_time - df$min_time) / 3600)
+df$ft_sum_hard_brakes_10_flg_val <-
+  df$ft_sum_hard_brakes_10_flg_val / df$duration
+df$ft_sum_hard_brakes_3_flg_val <-
+  df$ft_sum_hard_brakes_3_flg_val / df$duration
 
-#normalisation of features
-df$duration <- as.numeric((df$max_time - df$min_time)/3600)
 #df$ft_cnt_vehicle_deaccel_val <- df$ft_cnt_vehicle_deaccel_val/df$duration
-df$ft_sum_hard_brakes_10_flg_val <- df$ft_sum_hard_brakes_10_flg_val/df$duration
-df$ft_sum_hard_brakes_3_flg_val <- df$ft_sum_hard_brakes_3_flg_val/df$duration
 #df$ft_sum_time_deaccel_val <- df$ft_sum_time_deaccel_val/df$duration
 #df$ft_cnt_vehicle_accel_val <- df$ft_cnt_vehicle_accel_val/df$duration
 #df$ft_sum_hard_accel_10_flg_val <- df$ft_sum_hard_accel_10_flg_val/df$duration
@@ -126,74 +130,81 @@ df$ft_sum_hard_brakes_3_flg_val <- df$ft_sum_hard_brakes_3_flg_val/df$duration
 #df$ft_sum_time_accel_val <- df$ft_sum_time_accel_val/df$duration
 
 df <- as.data.table(df)
-df[,perc_hard_brakes_10:=ntile(ft_sum_hard_brakes_10_flg_val, 100)]
-df[,perc_hard_brakes_3:=ntile(ft_sum_hard_brakes_3_flg_val, 100)]
-df[,aggressiveness_score:=perc_hard_brakes_10 + perc_hard_brakes_3]
+df[, perc_hard_brakes_10 := ntile(ft_sum_hard_brakes_10_flg_val, 100)]
+df[, perc_hard_brakes_3 := ntile(ft_sum_hard_brakes_3_flg_val, 100)]
+df[, aggressiveness_score := perc_hard_brakes_10 + perc_hard_brakes_3]
 
-#remove fields
-df[,':='(duration = NULL
-         , min_time=NULL
-         , max_time=NULL
-         , vehicle_id=NULL
-         #, trip_id=NULL
-         , perc_hard_brakes_10=NULL
-         , perc_hard_brakes_3=NULL
-         #, ft_sum_hard_brakes_10_flg_val=NULL
-         , ft_sum_hard_brakes_3_flg_val=NULL
-         )]
+# Remove following features from data
+df[, ':='(
+  duration = NULL,
+  min_time = NULL,
+  max_time = NULL,
+  vehicle_id = NULL,
+  #trip_id=NULL,
+  perc_hard_brakes_10 = NULL,
+  perc_hard_brakes_3 = NULL,
+  #ft_sum_hard_brakes_10_flg_val=NULL,
+  ft_sum_hard_brakes_3_flg_val = NULL
+)]
 
 
-#####Split datasets and build model
+##----- Training model -----##
 
-#split datasets
+# Split data into train, validation, test
+set.seed(3451)
+inds <-
+  partition(df$aggressiveness_score, p = c(
+    train = 0.6,
+    valid = 0.2,
+    test = 0.2
+  ))
+str(inds)
 
-train <- df[1:1000]
-test <- df[900:1500]
-eval <- df[1400:1707]
+train <- df[inds$train, ]
+eval <- df[inds$valid, ]
+test <- df[inds$test, ]
 
-datasets <- list(trainData = train
-                 , testData = test
-                 , evalData =eval)
+datasets <- list(trainData = train,
+                 testData = test,
+                 evalData = eval)
 
 datasets <- convertyDataToXgBoost(datasets, 'aggressiveness_score')
 
-
 #XGB parameters
 paramxgBoost <- list(
-                    objective  = 'reg:squarederror' #
-                     , eval_metric = 'rmse'
-                     , eta =  0.01
-                     , subsample = 0.75 
-                     , colsample_bytree = 0.8076
-                     , min_child_weight = 16
-                     , max_depth = 3 
-                      )
+  objective  = 'reg:squarederror',
+  eval_metric = 'rmse',
+  eta =  0.01,
+  subsample = 0.75,
+  colsample_bytree = 0.8076,
+  min_child_weight = 16,
+  max_depth = 3
+)
 
 watchlist = list(train = datasets$trainData, eval = datasets$evalData)
 
-#Train model
-
-build_model <- function(train){
+# Train the model
+train_model <- function(train) {
+  model = xgb.train(
+    nrounds = 1000,
+    params = paramxgBoost,
+    data = train,
+    early_stop_round = 10,
+    watchlist = watchlist,
+    print_every_n = 50,
+    verbose = 1
+  )
   
-  model = xgb.train(nrounds = 1000, params = paramxgBoost
-                    , data = train
-                    , early_stop_round = 10
-                    , watchlist = watchlist
-                    , print_every_n = 50 
-                    , verbose = 1)
-  
-  
-  #generate predictions
-  res = predict(model, datasets$testData)
-  
-  return(res)  
+  return(model)
 }
 
+model <- train_model(datasets$trainData)
 
-preds <- build_model(datasets$trainData)
+# Generate predictions for test data
+preds = predict(model, datasets$testData)
 
-#evaluate the performance of the model
+# Evaluate the performance of the model on test data
+# Pearson Correlation
 evaluateModel(test$aggressiveness_score, preds)
-
-
-
+# RMSE
+rmse(test$aggressiveness_score, preds)
